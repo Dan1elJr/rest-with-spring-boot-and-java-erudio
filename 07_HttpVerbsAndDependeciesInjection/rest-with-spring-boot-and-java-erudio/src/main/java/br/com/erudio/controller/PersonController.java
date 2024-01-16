dd79e55e8f1b7d9b9224ddc33b89c17702246ae6
Daniel Junior <danieljunior1903@gmail.com>
2024-01-15 20:28:23 -0400

    Refactoring do Controller de Pessoas

--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -1,13 +1,16 @@
 package br.com.erudio.controller;
 
-import java.awt.PageAttributes.MediaType;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.model.Person;
@@ -21,34 +24,36 @@ public class PersonController {
 	private PersonServices service;
 	// private PersonServices service = new PersonServices();
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public List<Person> findAll() {
 
 		return service.findAll();
 	}
 
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@GetMapping(value = "/{id}")
 	public Person findById(@PathVariable(value = "id") Long id){
 		
 		return service.findById(id);
 	}
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public Person create(@RequestBody Person person) {
 		
 		return service.create(person);
 	}
 	
-	@RequestMapping(method = RequestMethod.PUT)
+	@PutMapping
 	public Person update(@RequestBody Person person) {
 		
 		return service.update(person);
 	}
 	
-	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-	public void delete(@PathVariable(value = "id") Long id){
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
 		
 		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
