743f30b2f1ff5101b9fd5901ffb5118fe7dcd4f1
Daniel Junior <danieljunior1903@gmail.com>
2024-01-18 18:40:54 -0400

    adding comments at DozerMapper.class

--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/mapper/DozerMapper.java
@@ -6,16 +6,20 @@ import java.util.List;
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
 
+//Classe Mapeadora de entidades
 public class DozerMapper {
-
+	
+	//Mapeador de entidades
 	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
-
+	
+	//Mapear entidade
 	public static <O, D> D parseObject(O origin, Class<D> destination) {
 
 		return mapper.map(origin, destination);
 
 	}
-
+	
+	//Mapear lista de entidadess
 	public static <O, D> List<D> parseListObject(List<O> origin, Class<D> destination) {
 		
 		List<D> destinationObjects = new ArrayList<D>();
