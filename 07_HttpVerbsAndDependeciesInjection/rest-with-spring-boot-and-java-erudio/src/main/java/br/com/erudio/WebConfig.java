b0c71fa70e0f7394fc047eae24ace0c0d35bfcad
Daniel <danieljunior1903@gmail.com>
2024-01-23 13:46:33 -0400

    mplementando Content Negotiation via QUERY Parameter

---
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/WebConfig.java
@@ -0,0 +1,26 @@
+package br.com.erudio;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	//Configurando o Server Driven do content negotiation
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		//https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
+		
+		configurer.favorParameter(true)
+		.parameterName("mediaType").ignoreAcceptHeader(true)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML);
+	}
+	
+	
+	
+	
+}
--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controller/PersonController.java
@@ -3,6 +3,7 @@ package br.com.erudio.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,38 +26,42 @@ public class PersonController {
 	private PersonServices service;
 	// private PersonServices service = new PersonServices();
 
-	@GetMapping
+	@GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public List<PersonVO> findAll() {
 
 		return service.findAll();
 	}
 
 
-	@GetMapping(value = "/{id}")
+	@GetMapping(value = "/{id}",produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO findById(@PathVariable(value = "id") Long id){
 		
 		return service.findById(id);
 	}
 	
-	@PostMapping
+	@PostMapping(consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_XML_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO create(@RequestBody PersonVO person) {
 		
 		return service.create(person);
 	}
 	
-	@PostMapping(value = "/v2")
+	@PostMapping(value = "/v2",
+			consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_XML_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) {
 		
 		return service.createV2(person);
 	}
 	
-	@PutMapping
+	@PutMapping(consumes= {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_XML_VALUE},
+			produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public PersonVO update(@RequestBody PersonVO person) {
 		
 		return service.update(person);
 	}
 	
-	@DeleteMapping(value = "/{id}")
+	@DeleteMapping(value = "/{id}",produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id){
 		
 		service.delete(id);
