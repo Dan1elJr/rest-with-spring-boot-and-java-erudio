57a6d1ccff376de396d8d56d42b563338968b9cc
djunior <djunior@digiboard.com.br>
2024-01-24 13:31:45 -0400

    Coding

--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/WebConfig.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/WebConfig.java
@@ -7,11 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
 	//Configurando o Server Driven do content negotiation
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 		//https://www.baeldung.com/spring-mvc-content-negotiation-json-xml
-		
+		/*
 		configurer.favorParameter(true)
 		.parameterName("mediaType")
 		.ignoreAcceptHeader(true)
@@ -19,5 +22,15 @@ public class WebConfig implements WebMvcConfigurer {
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
 		.mediaType("xml", MediaType.APPLICATION_XML);
+		*/
+		
+		configurer.favorParameter(false)
+		.ignoreAcceptHeader(false)
+		.useRegisteredExtensionsOnly(false)
+		.defaultContentType(MediaType.APPLICATION_JSON)
+		.mediaType("json", MediaType.APPLICATION_JSON)
+		.mediaType("xml", MediaType.APPLICATION_XML)
+		.mediaType("x-yaml", MEDIA_TYPE_APPLICATION_YML)
+		;
 	}	
 }
