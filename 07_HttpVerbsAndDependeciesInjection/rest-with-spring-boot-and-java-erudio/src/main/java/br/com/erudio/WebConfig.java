297fa96032c7d7555eb6e46410f7692f11db10e9
Daniel Junior <danieljunior1903@gmail.com>
2024-01-24 20:16:05 -0400

    Serializing in YML (YAML Ain't Markup Language)

--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/WebConfig.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/WebConfig.java
@@ -1,14 +1,25 @@
 package br.com.erudio;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.erudio.serialization.converter.YamlJackson2HttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
 	private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+	
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		
+		converters.add(new YamlJackson2HttpMessageConverter());	
+	}
 	
 	//Configurando o Server Driven do content negotiation
 	@Override
