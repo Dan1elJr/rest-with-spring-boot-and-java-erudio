f07e8533c76333727019cf1aac9cc7bc6c8d3a79
Daniel Junior <danieljunior1903@gmail.com>
2024-03-19 17:55:45 -0400

    Adicionando Suporte à HATEOAS às Demais Operações

--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -3,16 +3,20 @@ package br.com.erudio.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.github.dozermapper.core.Mapping;
 
 
 
 
-
+@JsonPropertyOrder({"id","firstName","lastName","address","gender"})
 public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@JsonProperty("id")
 	@Mapping("id")
 	private Long key;
 	private String firstName;
--- 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
+++ 07_HttpVerbsAndDependeciesInjection/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/services/PersonServices.java
@@ -32,7 +32,11 @@ public class PersonServices {
 
 		logger.info("Finding all people");
 
-		return DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
+		var persons =  DozerMapper.parseListObject(repository.findAll(), PersonVO.class);
+		persons
+		.stream()
+		.forEach(p-> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		return persons;
 	}
 
 	public PersonVO findById(Long id) {
@@ -42,7 +46,7 @@ public class PersonServices {
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
-		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		var vo = DozerMapper.parseObject(entity, PersonVO.class);
 		
 		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
 		
@@ -55,6 +59,7 @@ public class PersonServices {
 
 		var entity = DozerMapper.parseObject(person, Person.class);
 		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 
@@ -79,7 +84,7 @@ public class PersonServices {
 		entity.setGender(person.getGender());
 
 		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
-
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 
